a640780acf175e4de6c0ec8701876bc28aaa7c94
satyanathsatyanath <161452461+satyanathsatyanath@users.noreply.github.com>
2025-09-25 12:02:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Dockerized Flask App!'
+    return 'Hello, Dockerized Flask App Satya!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
+
